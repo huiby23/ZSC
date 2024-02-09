cf892a858af009ecc5f3d22629ba452972980507
PaladinEE15 <61980430+PaladinEE15@users.noreply.github.com>
2024-02-09 15:25:55 +0800

    add scripts for evaluating models

---
+++ pyhanabi/eval_models.py
@@ -0,0 +1,48 @@
+# This script is used to test agent performance
+
+import time
+import os
+import sys
+import pprint
+import pickle
+import numpy as np
+import torch
+from torch import nn
+
+from act_group import ActGroup
+from create import create_envs, create_threads
+from eval import evaluate, evaluate_saved_model
+import common_utils
+import rela
+import r2d2
+import utils
+
+####################### parameters setting #######################
+
+
+# define protagonist set here
+
+protagonist_pathset = ['test/ps5_pe50_t2_rp_mir02_seed0','test/ps5_pe50_t2_rp_mir0_seed0','test/ps5_pe50_t2_mir02_seed0','test/ps5_pe50_t2_mir0_seed0']
+
+# define partner set here
+partner_pathset = []
+name_set = ['iql','vdn','sad','aux']
+
+for type_idx in range(4):
+    for seed_idx in range(10):
+        partner_pathset.append(name_set[type_idx]+'/seed'+str(seed_idx))
+
+####################### experiment codes #######################
+torch.backends.cudnn.benchmark = True
+
+for prot_id, prot_path in enumerate(protagonist_pathset):
+    score_set = []
+    with open('verbose_out.txt', 'w') as f: # ignore verbose information
+        original_stdout = sys.stdout
+        sys.stdout = f
+        for part_id, part_path in enumerate(partner_pathset):
+            test_models = ['models/'+prot_path+'/model0.pthw','models/'+part_path+'/model0.pthw']
+            score, _, _, _, _ = evaluate_saved_model(test_models, 100, 0, 0)
+            score_set.append(score)
+        sys.stdout = original_stdout
+    print('prot id: ', prot_id, 'test complete, score:', np.mean(score_set), '+-', np.std(score_set))
