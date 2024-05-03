2d865e029d9d0b1f1b2bda5c935a7a9cb97802aa
PaladinEE15 <61980430+PaladinEE15@users.noreply.github.com>
2024-05-03 21:09:52 +0800

    Update eval_models.py
    add new functions

--- pyhanabi/eval_models.py
+++ pyhanabi/eval_models.py
@@ -19,6 +19,8 @@ import utils
 os.environ["CUDA_VISIBLE_DEVICES"] = "0" 
 ####################### parameters setting #######################
 
+show_xp = True
+show_1zsc = True
 
 # define protagonist set here
 protagonist_raw = ['obl_seed1','obl_seed2','obl_seed3','obl_seed4','obl_seed5']
@@ -36,18 +38,38 @@ for type_idx in range(4):
 ####################### experiment codes #######################
 torch.backends.cudnn.benchmark = True
 
-final_results = []
-for prot_id, prot_path in enumerate(protagonist_pathset):
-    score_set = []
-    with open('verbose_out.txt', 'w') as f: # ignore verbose information
-        original_stdout = sys.stdout
-        sys.stdout = f
-        for part_id, part_path in enumerate(partner_pathset):
-            if prot_path != part_path:
-                test_models = [prot_path, part_path]
-                score, _, _, _, _ = evaluate_saved_model(test_models, 500, 0, 0)
-                score_set.append(score)
-        sys.stdout = original_stdout
-    print('prot id: ', prot_id, 'test complete, score:', np.mean(score_set), '+-', np.std(score_set))
-    final_results.append(np.mean(score_set))
-print('final:',np.mean(final_results), '+-',np.std(final_results))
+if show_1zsc:
+    print('1-ZSC test start')
+    final_results = []
+    for _, prot_path in enumerate(protagonist_pathset):
+        score_set = []
+        with open('verbose_out.txt', 'w') as f: # ignore verbose information
+            original_stdout = sys.stdout
+            sys.stdout = f
+            for part_id, part_path in enumerate(partner_pathset):
+                if prot_path != part_path:
+                    test_models = [prot_path, part_path]
+                    score, _, _, _, _ = evaluate_saved_model(test_models, 500, 0, 0)
+                    score_set.append(score)
+            sys.stdout = original_stdout
+        print("Single Score: {:.2f}\pm{:.2f}".format(np.mean(score_set),np.std(score_set)))
+        final_results.append(np.mean(score_set))
+    print("Final Score: {:.2f}\pm{:.2f}".format(np.mean(final_results),np.std(final_results)))
+
+if show_xp:
+    print('xp test start')
+    final_results = []
+    for _, prot_path in enumerate(protagonist_pathset):
+        score_set = []
+        with open('verbose_out.txt', 'w') as f: # ignore verbose information
+            original_stdout = sys.stdout
+            sys.stdout = f
+            for _, part_path in enumerate(protagonist_pathset):
+                if prot_path != part_path:
+                    test_models = [prot_path, part_path]
+                    score, _, _, _, _ = evaluate_saved_model(test_models, 500, 0, 0)
+                    score_set.append(score)
+            sys.stdout = original_stdout
+        print("Single Score: {:.2f}\pm{:.2f}".format(np.mean(score_set),np.std(score_set)))
+        final_results.append(np.mean(score_set))
+    print("Final Score: {:.2f}\pm{:.2f}".format(np.mean(final_results),np.std(final_results)))
